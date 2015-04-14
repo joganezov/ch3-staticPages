1e3d14c3a4bb4b92610a1a1e4def0743172db549
Jeff Oganezov <joganezov@madisoncollege.edu>
2015-04-14 06:03:45 +0000

    what the fuck ever

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,46 +1,49 @@
 module SessionsHelper
 
-  # Logs in the given user.
-  def log_in(user)
-    session[:user_id] = user.id
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.digest(remember_token))
+    self.current_user = user
   end
-  
-  # Remembers a user in a persistent session.
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-  
-  # Returns the user corresponding to the remember token cookie.
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
-  end
-  
-  # Logs in the given user
-  def logged_in?
+
+  def signed_in?
     !current_user.nil?
   end
-  
-  # Forgets a persistent session.
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
+
+  def current_user=(user)
+    @current_user = user
   end
-    
-  # Logs out the current user.
-  def log_out
-    forget(current_user)
-    session.delete(:user_id)
-    @current_user = nil
-  end  
+
+  def current_user
+    remember_token  = User.digest(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def sign_out
+    current_user.update_attribute(:remember_token,
+                                  User.digest(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
 end
